9d058713ff8779706c5915da4ebb473ae9199dda
hongj <109964410+hongj97@users.noreply.github.com>
2024-04-11 13:49:19 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,14 +1,8 @@
 def solution(array, commands):
     answer = []
-    #sliced array
     for command in commands:
-        i = command[0]
-        j = command[1]
-        k = command[2]
-        
-        s_array = array[i-1:j]
-        # print(s_array)    #잘라온 array
-        s_array.sort()
+        i, j, k = command
+        s_array = sorted(array[i-1:j])
         answer.append(s_array[k-1])
     # print(answer)
     return answer
