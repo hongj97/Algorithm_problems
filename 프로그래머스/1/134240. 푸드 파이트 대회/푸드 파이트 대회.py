94538ac02d5e35a8ed8e17bcc99ad23566575642
hongj <109964410+hongj97@users.noreply.github.com>
2024-04-25 23:20:01 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.py
@@ -1,12 +1,6 @@
 def solution(food):
-    answer = ''
-    f_dict = {i:j for i, j in enumerate(food)}
     left = ''
-    for i in range(1, len(food)):
-        num = f_dict[i]
-        if num % 2 != 0:
-            num = num -1 # 개수가 홀수라면 1제외
-        left = left + str(i)*(num//2)
-    right = left[::-1]
-    answer = left + '0' + right
+    left_list = [left+str(i)*(food[i]//2) for i in range(1, len(food))]
+    left = ''.join(left_list)
+    answer = left + '0' + left[::-1]
     return answer
