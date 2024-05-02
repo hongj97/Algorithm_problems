dd989e20667a5cc86031359c563ccc65fd3260de
hongj <109964410+hongj97@users.noreply.github.com>
2024-05-02 21:35:08 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -0,0 +1,14 @@
+def solution(k, score):
+    answer = []
+    score_list= []
+    for i in range(len(score)):
+        if i<k: #k일까지 점수는 모두 추가
+            score_list.append(score[i])
+        else:
+            if min(score_list) < score[i]: # 새로운 점수가 크다면 
+                #가장 작은 수 제거 후 새로운 점수 추가
+                score_list.remove(min(score_list))
+                score_list.append(score[i])
+        answer.append(min(score_list))        
+    # print(answer)
+    return answer
