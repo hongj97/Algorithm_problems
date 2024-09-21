0cdabdddb9653a0b5c80d3dfbb5c71e7b287c66f
hongj <109964410+hongj97@users.noreply.github.com>
2024-09-21 23:54:49 +0900

    [Silver III] Title: N과 M (4), Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,15 @@
+def recur(num):
+    if num==m:
+        print(*arr)
+        return
+    for i in range(1, n+1):
+        if len(arr)>0 and i < arr[-1]:
+            continue
+        
+        arr.append(i)
+        recur(num+1)
+        arr.pop()
+
+n, m = map(int, input().split())
+arr=[]
+recur(0)
